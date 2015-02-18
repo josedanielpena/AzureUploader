e1a1dbbc0e780876cff3c80144a7aee214904552
josedanielpena <josedaniel@htm.com.co>
2015-02-18 17:01:26 -0500

    Subiendo la Clase CfgCnx.cs
    Esta clase falto

---
+++ Datos/CfgCnx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datos
+{
+    [Serializable]
+    public class CfgCnx
+    {
+
+        public string AccountName { get; set; }
+        public string AcoountKey { get; set; }
+        public string ContainerName { get; set; }
+        public string IdUsuario { get; set; }
+        public string KeyUsuario { get; set; }
+
+    }
+}
